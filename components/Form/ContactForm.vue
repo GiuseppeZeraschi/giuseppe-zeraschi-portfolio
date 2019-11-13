dc04585ae92debca095caad3ec6944ee15e50ec8
Giuseppe Zeraschi <giuseppe@hexdigital.com>
2019-11-13 11:31:42 +0000

    Added autocomplete attribute to inputs and active class to label

--- components/Form/ContactForm.vue
+++ components/Form/ContactForm.vue
@@ -9,6 +9,7 @@
             <div class="u-form__input-wrapper">
                 <label
                     for="firstName"
+                    :class="{ 'v-contact-form__label--active': inputActive(form.firstName) }"
                     class="v-contact-form__label"
                 >
                     First Name
@@ -16,14 +17,18 @@
                 <input
                     class="v-contact-form__input u-form__input"
                     id="firstName"
-                    v-model="form.firstName"
+                    v-model="form.firstName.value"
                     type="text"
+                    autocomplete="given-name"
                     required
+                    @focus="form.firstName.focus = true"
+                    @blur="form.firstName.focus = false"
                 >
             </div>
             <div class="u-form__input-wrapper">
                 <label
                     for="lastName"
+                    :class="{ 'v-contact-form__label--active': inputActive(form.lastName) }"
                     class="v-contact-form__label"
                 >
                     Last Name
@@ -31,9 +36,12 @@
                 <input
                     class="v-contact-form__input u-form__input"
                     id="lastName"
-                    v-model="form.lastName"
+                    v-model="form.lastName.value"
                     type="text"
+                    autocomplete="family-name"
                     required
+                    @focus="form.lastName.focus = true"
+                    @blur="form.lastName.focus = false"
                 >
             </div>
         </div>
@@ -41,6 +49,7 @@
             <div class="u-form__input-wrapper">
                 <label
                     for="phone"
+                    :class="{ 'v-contact-form__label--active': inputActive(form.phone) }"
                     class="v-contact-form__label"
                 >
                     Phone
@@ -48,13 +57,17 @@
                 <input
                     class="v-contact-form__input u-form__input"
                     id="phone"
-                    v-model="form.phone"
+                    v-model="form.phone.value"
                     type="tel"
+                    autocomplete="tel"
+                    @focus="form.phone.focus = true"
+                    @blur="form.phone.focus = false"
                 >
             </div>
             <div class="u-form__input-wrapper">
                 <label
                     for="email"
+                    :class="{ 'v-contact-form__label--active': inputActive(form.email) }"
                     class="v-contact-form__label"
                 >
                     Email
@@ -62,15 +75,19 @@
                 <input
                     class="v-contact-form__input u-form__input"
                     id="email"
-                    v-model="form.email"
+                    v-model="form.email.value"
                     type="email"
+                    autocomplete="email"
                     required
+                    @focus="form.email.focus = true"
+                    @blur="form.email.focus = false"
                 >
             </div>
         </div>
         <div class="u-form__input-wrapper">
             <label
                 for="message"
+                :class="{ 'v-contact-form__label--active': inputActive(form.message) }"
                 class="v-contact-form__label"
             >
                 Message
@@ -78,9 +95,11 @@
             <textarea
                 class="v-contact-form__text-area u-form__input"
                 id="message"
-                v-model="form.message"
+                v-model="form.message.value"
                 type="text"
                 required
+                @focus="form.message.focus = true"
+                @blur="form.message.focus = false"
             />
         </div>
         <button
@@ -103,16 +122,36 @@ export default {
                 'Playing hard to get?'
             ],
             form: {
-                firstName: '',
-                lastName: '',
-                phone: '',
-                email: '',
-                message: ''
+                firstName: {
+                    value: '',
+                    focus: false
+                },
+                lastName: {
+                    value: '',
+                    focus: false
+                },
+                phone: {
+                    value: '',
+                    focus: false
+                },
+                email: {
+                    value: '',
+                    focus: false
+                },
+                message: {
+                    value: '',
+                    focus: false
+                }
             }
         }
     },
     mounted() {
         this.headingIndex = Math.floor(Math.random() * Math.floor(3))
+    },
+    methods: {
+        inputActive(input) {
+            return !!input.value || input.focus;
+        }
     }
 }
 </script>
@@ -137,6 +176,11 @@ export default {
 
     &__label {
         margin-bottom: $spacing-tiny;
+
+        &--active {
+            color: $color-secondary;
+        }
+
     }
 
     &__input {
