88400aa863b197a4b113e96871110c6a4959f1cd
Giuseppe Zeraschi <giuseppe@hexdigital.com>
2020-05-24 18:03:09 +0100

    Added 404.html for netlify redirect

---
+++ layouts/error.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="v-error">
+        <Status>
+            Oops! Looks like this page doesn't exist...
+        </Status>
+    </div>
+</template>
+
+<script>
+import Status from '~/components/Status/Status.vue'
+
+export default {
+    layout: 'plain',
+    head() {
+        return {
+            title: 'Page not found...'
+        }
+    },
+    components: { Status },
+}
+</script>
